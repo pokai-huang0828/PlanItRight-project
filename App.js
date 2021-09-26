5641d4fde17da1896b0d4750d829be9636c519b4
Eric Cheung <eric1162004@hotmail.com>
2021-09-26 11:46:59 -0700

    firebase auth works

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+
+import Routes from "./app/navigation/index";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Let's get started</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Routes />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- app.config.js
+++ app.config.js
@@ -1,4 +1,6 @@
-{
+import 'dotenv/config';
+
+export default {
   "expo": {
     "name": "Plan-It-Right",
     "slug": "Plan-It-Right",
@@ -27,6 +29,15 @@
     },
     "web": {
       "favicon": "./app/assets/favicon.png"
+    },
+    extra: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+      measurementId: process.env.MEASUREMENT_ID
     }
   }
 }
